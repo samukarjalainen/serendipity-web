3058bf7067e72903ef8b5248db35f4a3595d7f6d
Samu Karjalainen <karjalainensamu@gmail.com>
2016-02-14 22:20:21 +0200

    Navbar now shows which item is active

---
+++ client/js/controllers/headerController.js
@@ -0,0 +1,6 @@
+app.controller('HeaderCtrl', ['$scope', '$location', function ($scope, $location) {
+  $scope.isActive = function (viewLocation) {
+    return viewLocation === $location.path();
+  }
+
+}]);
