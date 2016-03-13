45535f658d9cb0abe9cab936535bc4d272c5db15
Samu Karjalainen <karjalainensamu@gmail.com>
2016-03-13 17:55:41 +0200

    Nagivation bar updates
    - Navbar now gets hidden when scrolling down
    - Nabbar now shows when scrolling up

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,31 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
+  var previousScroll = 0;
 
+  // Hide or show navigation depending on scroll
+  $(window).scroll(function(){
+
+    var currentScroll = $(this).scrollTop();
+
+    if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
+
+      if (currentScroll > previousScroll){
+        hideNav();
+      } else {
+        showNav();
+      }
+
+      previousScroll = currentScroll;
+
+    }
+
+  });
+
+  function hideNav() {
+    $(".header > .navbar").removeClass("is-visible").addClass("is-hidden");
+  }
+  function showNav() {
+    $(".header > .navbar").removeClass("is-hidden").addClass("is-visible");
+  }
 
 });
-
