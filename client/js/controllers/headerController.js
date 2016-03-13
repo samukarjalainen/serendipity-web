4db4334f74e41ecee3dbc266359d25262e4a0ead
Samu Karjalainen <karjalainensamu@gmail.com>
2016-03-13 20:58:22 +0200

    Multiple fixes
    - Users and sounds now visible to 'admin' users only
    - Moved top margin from hero image to content wrapper

--- client/js/controllers/headerController.js
+++ client/js/controllers/headerController.js
@@ -1,6 +1,13 @@
 app.controller('HeaderCtrl', ['$scope', '$location', '$window', 'LoginFactory', function ($scope, $location, $window, LoginFactory) {
 
   $scope.loggedInUser = $window.localStorage.username;
+  $scope.admin = false;
+
+  if ($window.localStorage.userRole === 'admin') {
+    $scope.admin = true;
+  } else {
+    $scope.admin = false;
+  }
 
   $scope.isActive = function (viewLocation) {
     return viewLocation === $location.path();
