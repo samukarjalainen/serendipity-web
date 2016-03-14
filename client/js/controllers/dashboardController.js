3aa34d1d62a3afaad30afe6fc7ca83231a3cc424
zachpamp <zachpamp@vt.edu>
2016-03-14 13:00:19 +0200

    Dashboard changes

--- client/js/controllers/dashboardController.js
+++ client/js/controllers/dashboardController.js
@@ -1,6 +1,17 @@
 'use strict';
 
-app
-  .controller('DashboardCtrl', ['$scope', function ($scope) {
-
+app.controller('DashboardCtrl', ['$scope', '$http', function ($scope, $http) {
+	
+	$scope.sounds = [];
+	
+	$http.post('/api/sounds/mysounds').
+		then(function successCallback(successResponse) {
+			$scope.sounds = successResponse.data;
+			console.log(successResponse);
+		}, function errorCallback( errorResponse) {
+			console.log(errorResponse);
+			
+		});
+		console.log($scope.sounds);
+		
   }]);
