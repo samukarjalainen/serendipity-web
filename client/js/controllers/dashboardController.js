be0ee6ddceaee6b22e2764b35f2b803c6220b807
Samu Karjalainen <karjalainensamu@gmail.com>
2016-03-16 16:35:49 +0200

    Added btn-serendipity styling, open editor on click in dashboard

--- client/js/controllers/dashboardController.js
+++ client/js/controllers/dashboardController.js
@@ -1,17 +1,21 @@
 'use strict';
 
 app.controller('DashboardCtrl', ['$scope', '$http', function ($scope, $http) {
-	
+
 	$scope.sounds = [];
-	
+
 	$http.post('/api/sounds/mysounds').
 		then(function successCallback(successResponse) {
 			$scope.sounds = successResponse.data;
 			console.log(successResponse);
 		}, function errorCallback( errorResponse) {
 			console.log(errorResponse);
-			
+
 		});
 		console.log($scope.sounds);
-		
-  }]);
+
+  $scope.openEditor = function (sound) {
+    console.log(sound);
+  };
+
+}]);
